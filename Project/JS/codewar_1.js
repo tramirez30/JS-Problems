8d4306fe11f24ece756dcc6be0462d8156b2e690
tramirez30 <ramirez.tomas@yahoo.com>
2024-08-18 20:29:19 -0700

    Working on problem 1, setting up file

--- Project/JS/codewar_1.js
+++ Project/JS/codewar_1.js
@@ -1,1 +1,12 @@
-console.log("Hello World");
+// codewarsProblems.js
+
+// Namespace for all problems
+
+const codeWarProblems = {
+  // Problem 1: How good are you really? (8 kyu)
+  betterThanAverage(classPoints, yourPoints) {
+    console.log(classPoints, yourPoints);
+  },
+};
+
+codeWarProblems.betterThanAverage("Hello", "World!!!");
