aaa6df8eb23d224d4685f07c6678fa21bf383f66
tramirez30 <ramirez.tomas@yahoo.com>
2024-08-30 17:30:16 -0700

    Added syntax for new codewar problem

--- Project/JS/codewar_1.js
+++ Project/JS/codewar_1.js
@@ -73,6 +73,29 @@ const codeWarProblems = {
   },
 };
 
+// Problem 7: PaginationHelper - 5 kyu
+
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    // The constructor takes in an array of items and a integer indicating how many
+    // items fit within a single page
+  }
+  itemCount() {
+    // returns the number of items within the entire collection
+  }
+  pageCount() {
+    // returns the number of pages
+  }
+  pageItemCount(pageIndex) {
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of range
+  }
+  pageIndex(itemIndex) {
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+  }
+}
+
 // Test Case Problem #1 - How good are you really? (8 kyu)
 // codeWarProblems.betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75);
 
@@ -107,4 +130,6 @@ Note: 4 digits so 2 squared (2x2 perfect square). 2 digits on each line.
 // Push new value to empty Array
 // Return Array
 
-codeWarProblems.createArrayOfTiers(777);
+// codeWarProblems.createArrayOfTiers(777);
+
+// Test Case Problem #7 - PaginationHelper (5kyu)
