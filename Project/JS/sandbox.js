04c9cccda7c460933735e70e87de3c615cf0d343
tramirez30 <ramirez.tomas@yahoo.com>
2024-09-11 21:04:18 -0700

    Logging Symbol.iterator

--- Project/JS/sandbox.js
+++ Project/JS/sandbox.js
@@ -1,2 +1,5 @@
 let okIterator = "OK"[Symbol.iterator]();
-console.log(okIterator);
+console.log("Symbol", okIterator);
+console.log("Next", okIterator.next());
+console.log("NextNext", okIterator.next());
+console.log("NextNext", okIterator.next());
