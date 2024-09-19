76c4b140034258829060209424751721ddaa30af
tramirez30 <ramirez.tomas@yahoo.com>
2024-09-18 21:47:23 -0700

    Added minus() logic

--- Project/JS/main.js
+++ Project/JS/main.js
@@ -157,12 +157,14 @@ class Vec {
   plus(other) {
     return new Vec(this.x + other.x, this.y + other.y);
   }
-  minus() {}
+  minus(other) {
+    return new Vec(this.x - other.x, this.y - other.y);
+  }
 }
 let newVar = new Vec(1, 2).plus(new Vec(3, 4));
 console.log("Logic:", newVar);
 // → Vec{x: 3, y: 5}
-// console.log(new Vec(1, 2).minus(new Vec(2, 3)));
+console.log(new Vec(1, 2).minus(new Vec(2, 3)));
 // → Vec{x: -1, y: -1}
 // console.log(new Vec(3, 4).length);
 // → 5
