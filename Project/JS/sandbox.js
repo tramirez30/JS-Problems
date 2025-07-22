ac18d6ea6dbcafbb0a3ca62e84fbe42a40ced6d9
tramirez30 <ramirez.tomas@yahoo.com>
2025-07-21 21:31:26 -0700

    adding level 8kyu problems

--- Project/JS/sandbox.js
+++
@@ -1,7 +0,0 @@
-// Symbol.iterator() Exercise
-
-// let okIterator = "OK"[Symbol.iterator]();
-// console.log("Symbol", okIterator);
-// console.log("Next", okIterator.next());
-// console.log("NextNext", okIterator.next());
-// console.log("NextNext", okIterator.next());
