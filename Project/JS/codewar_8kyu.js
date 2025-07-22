3220849ac9d567ecec2e3fdd20497091ecad0cf5
tramirez30 <ramirez.tomas@yahoo.com>
2025-07-21 22:02:32 -0700

    working on 8kyu problem

--- Project/JS/codewar_8kyu.js
+++ Project/JS/codewar_8kyu.js
@@ -1,8 +1,22 @@
 const level8Kyu = {
   // Opposites attract - Level 8 kyu
   lovefunc(flower1, flower2) {
-    return (flower1 + flower2) % 2 === 0 ? true : false;
+    return (flower1 + flower2) % 2 === 0 ? false : true;
+  },
+  // Beginners Series #2 Clock
+  past(h, m, s) {
+    let total = 0;
+    if (0 <= h <= 23) {
+      total += h * 3600000;
+    }
+    if (0 <= m <= 59) {
+      total += (60 / m) * 1000;
+    }
+    if (0 <= s <= 59) {
+      total += s * 1000;
+    }
+    return total;
   },
 };
 
-console.log(level8Kyu.lovefunc(1, 1));
+console.log(level8Kyu.past(1, 0, 0));
