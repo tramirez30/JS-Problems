41fb8153a4fc2407f3b89bf263ee734af5fbc2b6
tramirez30 <ramirez.tomas@yahoo.com>
2025-07-21 21:37:51 -0700

    solving for problem level 8kyu

--- Project/JS/codewar_8kyu.js
+++ Project/JS/codewar_8kyu.js
@@ -1,1 +1,8 @@
-const level8kyu = {};
+const level8Kyu = {
+  // Opposites attract - Level 8 kyu
+  lovefunc(flower1, flower2) {
+    return (flower1 + flower2) % 2 === 0 ? true : false;
+  },
+};
+
+console.log(level8Kyu.lovefunc(1, 1));
