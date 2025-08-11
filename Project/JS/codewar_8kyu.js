81d5a75466df23bed916bba24f2f74df1bd5d315
tramirez30 <ramirez.tomas@yahoo.com>
2025-08-10 17:59:05 -0700

    Solving for REduce but Grow problem

--- Project/JS/codewar_8kyu.js
+++ Project/JS/codewar_8kyu.js
@@ -48,9 +48,18 @@ const level8Kyu = {
     return total / array.length;
   },
   // Beginner - Reduce but Grow
+  grow(array) {},
 };
 
-console.log(level8Kyu.findAverage([1, 2, 3]));
+console.log(level8Kyu.grow([1, 2, 3]));
 /*
+Description:
+
+Given a non-empty array of integers, return the result of multiplying 
+the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+grow([1, 2, 3]), 6
 
 */
