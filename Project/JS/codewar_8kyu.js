9f20dfbacd59c1a2239968afaaf5d8cfe004317d
tramirez30 <ramirez.tomas@yahoo.com>
2025-09-29 21:51:43 -0700

    Added new Kyu problem

--- Project/JS/codewar_8kyu.js
+++ Project/JS/codewar_8kyu.js
@@ -116,9 +116,21 @@ const level8Kyu = {
     } while (num > 0);
     return arr;
   },
+  // Rock Paper Scissors
+  rps(p1, p2) {
+    if (p1 === "rock" && p2 === "scissors") {
+      console.log("Player 1 won!");
+    } else if (p1 === "paper" && p2 === "rock") {
+      console.log("Player 1 won!");
+    } else if (p1 === "scissors" && p2 === "rock") {
+      console.log("Player 2 won!");
+    } else if (p1 === p2) {
+      return "Draw!";
+    }
+  },
 };
 
-console.log(level8Kyu.reverseSeq(5));
+console.log(level8Kyu.rps("scissors", "rock"));
 
 /*
 P - Array of numbers that are positive & negative
