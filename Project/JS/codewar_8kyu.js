f2c018cdec512d017a6e1be685d3de9a968385dd
tramirez30 <ramirez.tomas@yahoo.com>
2025-10-02 21:37:13 -0700

    refactoring code

--- Project/JS/codewar_8kyu.js
+++ Project/JS/codewar_8kyu.js
@@ -118,18 +118,14 @@ const level8Kyu = {
   },
   // Rock Paper Scissors
   rps(p1, p2) {
-    if (p1 === "rock" && p2 === "scissors") {
-      console.log("Player 1 won!");
-    } else if (p1 === "paper" && p2 === "rock") {
-      console.log("Player 1 won!");
-    } else if (p1 === "scissors" && p2 === "rock") {
-      console.log("Player 2 won!");
-    } else if (p1 === "rock" && p2 === "paper") {
-      console.log("Player 2 won!");
-    } else if (p1 === "paper" && p2 === "scissors") {
-      console.log("Player 2 won!");
-    } else if (p1 === p2) {
-      return "Draw!";
+    const options = {
+      rock: "scissor",
+      paper: "rock",
+      scissor: "paper",
+    };
+
+    if (p1 === p2) {
+      console.log("Draw");
     }
   },
 };
